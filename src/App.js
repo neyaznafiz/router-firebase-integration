0d81266513dbce3315ad40b8a8857a1ab4701c5a
niiyazobug <niiyaz5555@gmail.com>
2022-04-09 02:31:03 +0600

    header nav a css added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
+
+      <Header></Header>
 
       <Routes>
 
