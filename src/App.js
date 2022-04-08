8a47bc243ee1e24366bdd3617661769ed886d8c5
niiyazobug <niiyaz5555@gmail.com>
2022-04-09 02:22:19 +0600

    component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
 
-      
-     
+      <Routes>
+
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+
+      </Routes>
+
     </div>
   );
 }
