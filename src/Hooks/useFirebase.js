f8fc632196878d2f36e9e709ba9fb3bd50efc6d5
niiyazobug <niiyaz5555@gmail.com>
2022-04-09 12:16:03 +0600

    setuser added

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -20,6 +20,7 @@ const useFirebase = () => {
         signInWithPopup(auth, googleProvider)
         .then(result =>{
             const user = result.user
+            setUser(user)
             console.log(user);
         })
     }
