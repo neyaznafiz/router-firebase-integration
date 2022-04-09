4b0bf261ce86793c5fe616012277fcdbc0aac813
niiyazobug <niiyaz5555@gmail.com>
2022-04-09 16:19:57 +0600

    orders components added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
+import Orders from './Components/Orders/Orders';
 import Products from './Components/Products/Products';
 import Register from './Components/Register/Register';
 
@@ -19,6 +20,7 @@ function App() {
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/orders' element={<Orders></Orders>}></Route>
 
       </Routes>
 
---
+++ src/Components/Orders/Orders.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Order.css'
+
+const Orders = () => {
+    return (
+        <div>
+            <h2>This is order</h2>
+        </div>
+    );
+};
+
+export default Orders;
