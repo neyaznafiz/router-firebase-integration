bd95d55cb268da598dc96f0459ad30cb5ea387b4
niiyazobug <niiyaz5555@gmail.com>
2022-04-09 15:29:44 +0600

    display user info in home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import useFirebase from '../../Hooks/useFirebase';
 import './Home.css'
 
 const Home = () => {
+
+    const { user } = useFirebase()
+
+
     return (
         <div>
             <h2>This is Home</h2>
+
+            <div>
+            <p>Current user is :- </p>
+                {
+                    user ?
+                    <>
+                    <img src={user.photoURL} alt="" />
+                     <h5>Name : {user.displayName}</h5>
+                     <p>E-mail : {user.email}</p> 
+                    </> 
+                    : 'No body here'
+                }
+            
+                </div>
         </div>
     );
 };
